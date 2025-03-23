fa4f47725cb106190c58878a35667596b18f5f0d
woni <165345087+HawonNam@users.noreply.github.com>
2025-03-23 19:03:00 +0900

    Update info.py
    1. 조원 - 남하원 정보 추가
    2. 변수명 수정

--- info.py
+++ info.py
@@ -1,3 +1,7 @@
-name = "leeshinbi"
-student_id = "202400555"
-department = "Computer Science and Engineering"
+name_1 = "leeshinbi"
+student_id_1 = "202400555"
+department_1 = "Computer Science and Engineering"
+
+name_2 = "HawonNam"
+student_id_2 = "202402754"
+department_2 = "Computer Science and Engineering"
